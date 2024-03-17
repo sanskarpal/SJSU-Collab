aa3534dfa1194b3fa6384fc384824f3cec4e026c
juiceman1014 <justintran1014@gmail.com>
2024-03-16 23:12:48 -0700

    added routing for signup and login page

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from './SignUp';
+import LoginPage from './LoginPage'; // Make sure to create this component
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-   <SignUp/>
-  </React.StrictMode>,
-)
-
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignUp />} /> {/* Root path now points to SignUp */}
+        <Route path="/signup" element={<SignUp />} /> {/* Redundant but you can keep it if you want */}
+        <Route path="/login" element={<LoginPage />} /> {/* Login page route */}
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
