0e25953adb14d45cb5f31ab4c4dc679779cf5158
juiceman1014 <justintran1014@gmail.com>
2024-03-17 20:56:10 -0700

    added routing for the home page

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from './SignUp';
-import LoginPage from './LoginPage'; // Make sure to create this component
+import LoginPage from './LoginPage';
+import HomePage from './HomePage'; // Make sure this import matches your file structure
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<SignUp />} /> {/* Root path now points to SignUp */}
-        <Route path="/signup" element={<SignUp />} /> {/* Redundant but you can keep it if you want */}
-        <Route path="/login" element={<LoginPage />} /> {/* Login page route */}
-        {/* Add more routes as needed */}
+        <Route path="/" element={<HomePage />} /> {/* HomePage at the root path */}
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LoginPage />} />
+        {/* You can add more routes here as needed */}
       </Routes>
     </Router>
   </React.StrictMode>
