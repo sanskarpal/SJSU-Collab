b3da2ff8430c0d06f40523cbfa2f7ba3283ccde6
juiceman1014 <justintran1014@gmail.com>
2024-04-23 00:46:24 -0700

    logout button now functional and user pfp image is persisted in header as well

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Header.css'; // Make sure you have corresponding CSS file for styles
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getDatabase, ref as databaseRef, get } from 'firebase/database';
+import './Header.css';
 
 const Header = () => {
+  const [userProfileImage, setUserProfileImage] = useState('default-profile-image-url');
+  const auth = getAuth();
+  const database = getDatabase();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const uid = user.uid;
+        // Fetch the user's profile image URL from the database
+        const userProfileRef = databaseRef(database, 'users/' + uid);
+        get(userProfileRef).then((snapshot) => {
+          if (snapshot.exists()) {
+            const data = snapshot.val();
+            setUserProfileImage(data.imageURL || 'default-profile-image-url');
+          }
+        }).catch((error) => {
+          console.error(error);
+        });
+      }
+    });
+  }, [auth, database]);
+  
   return (
     <header>
       <img 
@@ -12,17 +37,17 @@ const Header = () => {
       <h1>SJSU COLLAB</h1>
       <div className="dropdown">
         <button className="dropbtn">
-          <img 
-            id="profile-image" 
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzdWHcffKPDbUMWEVLor3x7sknODQ7SP-Qmw&usqp=CAU" 
-            alt="Profile"
-          />
+        <img
+          id="profile-image"
+          src={userProfileImage}
+          alt="Profile"
+          onError={(e) => { e.target.onerror = null; e.target.src = 'default-profile-image-url'; }}
+        />
         </button>
         <div className="dropdown-content">
           <p><Link to="/profile">Profile</Link></p>
           <p><Link to="/">Home</Link></p>
-          <p><Link to="/login">Sign In</Link></p>
-          <a href="#">Logout</a>
+          <p><Link to="/login">Sign Out</Link></p>
         </div>
       </div>
     </header>
